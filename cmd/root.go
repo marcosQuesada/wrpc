d4241c9f789da27b71be38588563ca513dd9f266
marcosQuesada <marcos.quesadas@gmail.com>
2020-09-29 15:45:45 +0200

    base project as regular grpc

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+)
+
+
+var rootCmd = &cobra.Command{
+	Use:   "wrpc service",
+	Short: "wrpc service cli",
+	Long:  "wrpc service cli",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Unexpected execute error, err %v", err)
+		os.Exit(1)
+	}
+}
+
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/marcosQuesada/wrpc
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.1
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.5 // indirect
+	google.golang.org/grpc v1.21.0
+)
